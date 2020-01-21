de7178a13baf64ac2d6cd22300f3aabc4e891e1d
Martin Kouba <mkouba@redhat.com>
2020-01-21 14:14:51 +0100

    Step 1 - add FeatureBuildItem

--- extension/deployment/src/main/java/org/acme/watcher/deployment/WatcherBuildSteps.java
+++ extension/deployment/src/main/java/org/acme/watcher/deployment/WatcherBuildSteps.java
@@ -1,5 +1,13 @@
 package org.acme.watcher.deployment;
 
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
 public class WatcherBuildSteps {
+    
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem("watcher");
+    }
 
 }
